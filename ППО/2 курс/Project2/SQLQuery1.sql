212fa72cc978cd777feca09ebb0b4e08433a35a2
Azizjan-leo <litpulla@mail.ru>
2017-01-15 21:09:35 +0600

    add Software Developing

---
+++ ППО/2 курс/Project2/SQLQuery1.sql
@@ -0,0 +1,8 @@
+use AziClinic
+go
+create table SymptomsDiagnosis
+(
+Id int primary key identity,
+Symptom int,
+Diagnosis int
+)
---
+++ ППО/2 курс/Project2/WindowsApplication1/WindowsApplication1/Symptoms.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Symptoms
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.NameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DesriptionDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.SymptomsBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.AziClinicDataSet = New WindowsApplication1.AziClinicDataSet()
+        Me.SymptomsTableAdapter = New WindowsApplication1.AziClinicDataSetTableAdapters.SymptomsTableAdapter()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SymptomsBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.AziClinicDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.NameDataGridViewTextBoxColumn, Me.DesriptionDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.SymptomsBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(13, 40)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(399, 170)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'NameDataGridViewTextBoxColumn
+        '
+        Me.NameDataGridViewTextBoxColumn.DataPropertyName = "Name"
+        Me.NameDataGridViewTextBoxColumn.HeaderText = "Name"
+        Me.NameDataGridViewTextBoxColumn.MinimumWidth = 150
+        Me.NameDataGridViewTextBoxColumn.Name = "NameDataGridViewTextBoxColumn"
+        Me.NameDataGridViewTextBoxColumn.Width = 150
+        '
+        'DesriptionDataGridViewTextBoxColumn
+        '
+        Me.DesriptionDataGridViewTextBoxColumn.DataPropertyName = "Desription"
+        Me.DesriptionDataGridViewTextBoxColumn.HeaderText = "Desription"
+        Me.DesriptionDataGridViewTextBoxColumn.MinimumWidth = 150
+        Me.DesriptionDataGridViewTextBoxColumn.Name = "DesriptionDataGridViewTextBoxColumn"
+        Me.DesriptionDataGridViewTextBoxColumn.Width = 150
+        '
+        'SymptomsBindingSource
+        '
+        Me.SymptomsBindingSource.DataMember = "Symptoms"
+        Me.SymptomsBindingSource.DataSource = Me.AziClinicDataSet
+        '
+        'AziClinicDataSet
+        '
+        Me.AziClinicDataSet.DataSetName = "AziClinicDataSet"
+        Me.AziClinicDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'SymptomsTableAdapter
+        '
+        Me.SymptomsTableAdapter.ClearBeforeFill = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(138, 217)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(193, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(337, 216)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 21)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Ok"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Modern No. 20", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(84, 222)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(48, 15)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Search"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(347, 11)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(65, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "New"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Modern No. 20", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(154, 16)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(102, 18)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "SYMPTOMS"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(13, 217)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(53, 19)
+        Me.Button3.TabIndex = 6
+        Me.Button3.Text = "Refresh"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Symptoms
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(424, 259)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.MaximizeBox = False
+        Me.Name = "Symptoms"
+        Me.Text = "AziClinic"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.SymptomsBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.AziClinicDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents AziClinicDataSet As AziClinicDataSet
+    Friend WithEvents SymptomsBindingSource As BindingSource
+    Friend WithEvents SymptomsTableAdapter As AziClinicDataSetTableAdapters.SymptomsTableAdapter
+    Friend WithEvents NameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents DesriptionDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Button3 As Button
+End Class
