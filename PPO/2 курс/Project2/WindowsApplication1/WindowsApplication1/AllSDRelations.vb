8ca4c15be9a8eede71b9fdceb742b3a2af5f322f
Azizjan-leo <litpulla@mail.ru>
2017-02-01 16:26:21 +0600

    New Start (adding PPO)

---
+++ PPO/2 курс/Project2/WindowsApplication1/WindowsApplication1/AllSDRelations.vb
@@ -0,0 +1,15 @@
+﻿Public Class AllSDRelations
+    Private Sub AllSDRelations_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'AziClinicDataSet.SD' table. You can move, or remove it, as needed.
+        Me.SDTableAdapter.Fill(Me.AziClinicDataSet.SD)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        SymptomDiagnos.Show()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.SDTableAdapter.Fill(Me.AziClinicDataSet.SD)
+    End Sub
+End Class
---
+++ PPO/2 курс/Project2/WindowsApplication1/WindowsApplication1/NewDiagnosis.vb
@@ -0,0 +1,21 @@
+﻿Public Class NewDiagnosis
+    Private Sub DiagnosisBindingNavigatorSaveItem_Click(sender As Object, e As EventArgs) Handles DiagnosisBindingNavigatorSaveItem.Click
+        Me.Validate()
+        Me.DiagnosisBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.AziClinicDataSet)
+    End Sub
+
+    Private Sub NewDiagnosis_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'AziClinicDataSet.Diagnosis' table. You can move, or remove it, as needed.
+        Me.DiagnosisTableAdapter.Fill(Me.AziClinicDataSet.Diagnosis)
+        DiagnosisBindingSource.AddNew()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Validate()
+        Me.DiagnosisBindingSource.EndEdit()
+        Me.TableAdapterManager.UpdateAll(Me.AziClinicDataSet)
+        Diagnoses.DiagnosisTableAdapter.Fill(Me.AziClinicDataSet.Diagnosis)
+        Me.Close()
+    End Sub
+End Class
